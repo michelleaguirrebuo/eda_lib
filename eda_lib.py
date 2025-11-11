bbf6e5a6209dcb5d2b87e592433a27e1416a9e47
michelleaguirrebuo <michelleaguirre@getbuo.com>
2025-11-11 00:00:06 -0600

    Update eda_lib.py

--- eda_lib.py
+++ eda_lib.py
@@ -70,7 +70,7 @@ class RadarHeatmap:
         # Figure setup
         fig, ax = plt.subplots(subplot_kw={"projection": "polar"}, figsize=figsize)
         ax.set_xticks(theta[:-1])
-        ax.set_xticklabels(features, fontsize=12)
+        #ax.set_xticklabels(features, fontsize=12)
         ax.set_yticklabels([])
         ax.tick_params(axis='x', direction='out', pad=50)
 
