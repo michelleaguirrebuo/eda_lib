9ce9171c1e5f84bb541de676a19968699447cc61
michelleaguirrebuo <michelleaguirre@getbuo.com>
2025-11-10 23:18:31 -0600

    Update eda_lib.py

--- eda_lib.py
+++ eda_lib.py
@@ -70,8 +70,9 @@ class RadarHeatmap:
         # Figure setup
         fig, ax = plt.subplots(subplot_kw={"projection": "polar"}, figsize=figsize)
         ax.set_xticks(theta[:-1])
-        ax.set_xticklabels(features, fontsize=12, pad=15)
+        ax.set_xticklabels(features, fontsize=12)
         ax.set_yticklabels([])
+        ax.tick_params(axis='x', pad=15)
 
         # Colormaps for different groups
         cmaps = ["plasma", "viridis", "magma", "cividis", "inferno"]
