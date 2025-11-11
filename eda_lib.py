e4bd7983a2d0b1f2c4591eeba53ce9638041f340
michelleaguirrebuo <michelleaguirre@getbuo.com>
2025-11-11 11:17:36 -0600

    Update eda_lib.py

--- eda_lib.py
+++ eda_lib.py
@@ -195,7 +195,7 @@ class EDA:
               pass
         cols=self.df.columns
         for col in cols:
-            self.df.rename({col:col.lower().removeprefix('radarpsychometric').removesuffix('psychometric')})
+            self.df.rename({col:col.lower().removeprefix('radarpsychometric').removesuffix('psychometric')},axis=1,inplace=True)
 
     def visualize(self,columns:list or str=[],rows=5, head=True, tail=False):
         '''
